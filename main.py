ef6246535d3be611c15ed7b4c0c7de97314d39bf
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2018-11-13 21:16:33 +0530

    Removed reserved var from the code

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import csv
 
-reserved = [
-  "Access count"
-];
-
 data = {
   "WhatsApp": [
     {
@@ -17,24 +13,20 @@ with open("data/AUM_V4_Usage_2018-11-13_19-27-59.csv", "rb") as csvfile:
   reader = csv.reader(csvfile, delimiter=",", quotechar="|")
   for row in reader:
     try:
-      if str(row[2]) not in reserved:
-        _accessCount = int(row[2].replace("\"", ""));
-        _app = row[0].replace("\"", "");
-        _timeElapsed = row[1].replace("\"", "");
-        
-        # App data object;
-        _appData = {
-          "accessCount": _accessCount,
-          "timeElapsed": _timeElapsed
-        };
+      _accessCount = int(row[2].replace("\"", ""));
+      _app = row[0].replace("\"", "");
+      _timeElapsed = row[1].replace("\"", "");
+      
+      # App data object;
+      _appData = {
+        "accessCount": _accessCount,
+        "timeElapsed": _timeElapsed
+      };
 
-        try:
-          data[_app].append(_appData);
-        except KeyError:
-          data[_app] = [_appData];
-
-      else:
-        print("Hello");
+      try:
+        data[_app].append(_appData);
+      except KeyError:
+        data[_app] = [_appData];
 
     except IndexError:
       pass;
